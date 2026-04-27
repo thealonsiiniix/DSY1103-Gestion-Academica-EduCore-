ada3f5d0426f7730d3d42ab527c61465ddd22e54
thealonsiiniix <al.contrerasn@duocuc.cl>
2026-04-27 13:46:05 -0400

    ms.docente

---
+++ ms.docente/ms.docente/src/main/java/cl/instituto/pacifico/ms_docentes/model/Docente.java
@@ -0,0 +1,42 @@
+package cl.instituto.pacifico.ms_docentes.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "docentes")
+public class Docente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rut;
+    private String nombre;
+    private String apellido;
+    private String correo;
+
+    public Docente() {}
+
+    public Docente(Long id, String rut, String nombre, String apellido, String correo) {
+        this.id = id;
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getRut() { return rut; }
+    public void setRut(String rut) { this.rut = rut; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getApellido() { return apellido; }
+    public void setApellido(String apellido) { this.apellido = apellido; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+}
