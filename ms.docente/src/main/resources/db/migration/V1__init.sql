6c24848216b5dea99fa128af4d9e0eecb92aac23
sebarros <alexbsebas061@gmail.com>
2026-06-22 10:03:43 -0400

    fix(ms-docente): enable Flyway and fix database initialization for H2

--- ms.docente/src/main/resources/db/migration/V1__init.sql
+++ ms.docente/src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,12 @@
-CREATE TABLE docentes (
-                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                          rut VARCHAR(20) UNIQUE,
-                          nombre VARCHAR(100) NOT NULL,
-                          apellido VARCHAR(100) NOT NULL,
-                          correo VARCHAR(150) UNIQUE
+CREATE TABLE DOCENTES (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    rut VARCHAR(20) UNIQUE,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    correo VARCHAR(150) UNIQUE
 );
 
-INSERT INTO docentes (rut, nombre, apellido, correo) VALUES
+INSERT INTO DOCENTES (rut, nombre, apellido, correo) VALUES
                                                          ('11111111-1', 'Carlos', 'Muñoz', 'carlos@instituto.cl'),
                                                          ('22222222-2', 'María', 'González', 'maria@instituto.cl'),
                                                          ('33333333-3', 'Pedro', 'Rojas', 'pedro@instituto.cl'),
